9b95f754c5fd00fc85ea755e2eb518eaf50a6c6c
shishi2311 <147653988+shishi2311@users.noreply.github.com>
2025-12-26 00:26:51 +0530

    Add new frontend URL to CORS middleware

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8080", "http://localhost:8080"],  # ✅ your frontend URLs
+    allow_origins=["http://127.0.0.1:8080", "http://localhost:8080", "https://atal-bhujal.vercel.app"],  # ✅ your frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
